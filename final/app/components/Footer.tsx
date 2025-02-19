041a1bbf40ab2c04c954c7f137c968bc630d9e0c
luchernan <lukihernandez2005@gmail.com>
2025-02-19 13:56:57 +0100

    1 commit 2 repositorio

---
+++ final/app/components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="py-3">
+    <div className="m-5">
+        <div className="flex justify-between items-center">
+            <div className="text-gray-500 mt-1">
+                Daniel Sánchez and Lucas Hernández 2ºDAW
+            </div>
+            <div>
+                <img src="img/Spotify_Full_Logo_RGB_White.png" alt="Spotify Logo" className="w-30" />
+            </div>
+        </div>
+    </div>
+</footer>
+)
+  
+}
+
+export default Footer
+
